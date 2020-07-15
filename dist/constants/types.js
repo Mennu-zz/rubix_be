86f3c5151210c48eef6c582041cd8a6251351a65
Naveen Meher Chintala <naveenmeher.chintala@ada.com>
2020-07-16 01:34:30 +0200

    Updated gitIgnore

--- dist/constants/types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var TYPES = {
-    TradeController: Symbol.for('TradeController'),
-    TradeService: Symbol.for("TradeService"),
-};
-exports.default = TYPES;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidHlwZXMuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvY29uc3RhbnRzL3R5cGVzLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQUEsSUFBTSxLQUFLLEdBQUc7SUFDVixlQUFlLEVBQUUsTUFBTSxDQUFDLEdBQUcsQ0FBQyxpQkFBaUIsQ0FBQztJQUM5QyxZQUFZLEVBQUUsTUFBTSxDQUFDLEdBQUcsQ0FBQyxjQUFjLENBQUM7Q0FDM0MsQ0FBQTtBQUVELGtCQUFlLEtBQUssQ0FBQyJ9
